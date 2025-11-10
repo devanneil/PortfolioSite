33c81106c3491b8c30716815c452ede16e99099d
Devan Neil <devanneil@mines.edu>
2025-11-10 12:34:00 -0700

    Pushing temporary website

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
+import headshot from './pictures/Headshot.png'
+function Headshot() {
+  return <img src={headshot} alt="Headshot image" />
+}
 export default function Square() {
-  return <button className="square">X</button>;
+  return <div>
+    <h1>Devan Neil</h1>
+    <p1>
+      This is a placeholder for my portfolio website.
+      I am currently in the process of learning React to make this look better.
+      "How do we change the world? One step at a time."
+    </p1>
+    <Headshot /> <br />
+    <p2>
+      <a href="https://github.com/devanneil" target="_blank" rel="noopener noreferrer">
+        Visit my GitHub
+      </a>
+    </p2>
+    <br />
+    <p2>
+      <a href="www.linkedin.com/in/devan-neil" target="_blank" rel="noopener noreferrer">
+        Visit my LinkedIn
+      </a>
+    </p2>
+
+  </div>
 }
