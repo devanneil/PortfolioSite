f972c26c01e2697791a27a302acacafb20632cc0
Devan Neil <devanneil@mines.edu>
2026-01-13 15:46:13 -0700

    Added information to Projects.

--- src/Projects.js
+++ src/Projects.js
@@ -3,13 +3,15 @@ import TitleBar from './titleBar'
 import ContentBlock from './contentBlock'
 import HMFrame from './pictures/HMFrame.png'
 import Logo from './pictures/LogoSmall.png'
+import LegoRobot from './pictures/LegoRobot.png'
+import NAOPDF from './assets/Investigating the Correlation Between Robot Informality and Ability to Affect Patient Anxiety.docx.pdf'
 export default function Projects() {
   return <div className='contentBody'>
     <TitleBar title="Project Gallery" details={"A comprehensive list of my engineering projects"} imageSrc={Robot} selectedPage="Projects" imageAlt="Headshot"/>
     <ContentBlock>
       <h1 className='sectionHeader' style={{ fontSize: '4rem'}}>Robotics Projects</h1>
       <p className='contentSection'>
-        Projects listed by size and interest
+        Projects listed by size and scope
       </p>
     </ContentBlock >
 
@@ -70,25 +72,150 @@ export default function Projects() {
       
     </ContentBlock>
 
+    <ContentBlock image={Logo} imageAlt="The CAD for the Krap Shoot" caption='The CAD for the Paddock Pal V.2, this is not reflective of the final design'> 
+      <div>
+        <h1 className='sectionHeader'>Krap Shoot</h1>
+        <p className='contentSection'>
+          The Krap Shoot is my Capstone Design project. 
+          Together with a team of 13 other engineering students, we are automating the process of paddock mucking.
+          The more official name of this project is called the Paddock Pal V.2.
+          The goal of this project is to automatically identify manure piles inside a paddock, clean them, and deposit the waste in a collection area.
+          This project is in association with {" "}
+          <a
+            href="https://www.mines.edu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           Longhopes Donkey Shelter 
+          </a>
+          {" "}and the {" "}
+          <a
+            href="https://www.mines.edu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           Colorado School of Mines
+          </a>.
+          <br/><br/>
+          When we first began this project we had the parts from V.1.
+          V.1 was a similar project taken by previous teams, which was not able to be completed.
+          The design of the Krap Shoot is a skid steer front wheel drive vehicle with a scoop on the front. 
+          Attached is an image of our CAD, which I am not responsible for.
+          <br/><br/>
+          I am in charge of the software system. 
+          In our team we have 2 robotics students, myself included, and a team of 3 software students who were in a field session program.
+          Our Field Session team was tasked with building a pathfinding algorithm for our robot to handle navigation from point to point and planning the path to complete a job.
+          It is my responsibility to manage communications between these teams, and to handle implementation of the various systems.
+          <br/><br/>
+          Our robot has LiDAR, GPS, IMU and a Camera.
+          Using all of these Krap Shoot will be able to navigate a paddock and complete jobs autonomously.
+          We use the GPS to track the boundaries of the paddock, or the working area.
+          We use the LiDAR and the integrated IMU to handle tracking hazards, as paddocks are occupied, and for handling position feedback.
+          <br/><br/>
+          This project is still ongoing and is the capstone project for my engineering program.
+        </p>
+      </div>
+      
+    </ContentBlock>
+
+    <ContentBlock image={LegoRobot} imageAlt='A robotic car made out of legos' caption='The robotic car for my robotics class, it has trapped itself'> 
+      <div>
+        <h1 className='sectionHeader' style={{ fontSize: '3rem'}}>Other Robotics Projects</h1>
+        <h1 className='sectionHeader'>Lego Robotics</h1>
+        <p className='contentSection'>
+          As part of my robotics curriculum, I was tasked with building an arduino robot to solve a maze.
+          This robot used mostly LEGO parts, along with an arduino, breadboard, 2 break beam sensors, a sonar sensor and an ir sensor.
+          The goal was to navigate the maze grid autonomously.
+          This goal was never achieved due to the flimsy nature of the parts, however I did learn about motor classification, sensor calibration and a maze navigation algorithm.
+        </p>
+        <h1 className='sectionHeader'>NAO Research Project</h1>
+        <p className='contentSection'>
+          As more of my robotics curriculum, I needed to study how robots can interact with humans in a more social setting.
+          For this task my group was given the NAO robot.
+          We attempted to apply the NAO robot to treat anxiety caused by healthcare.
+          You can see our report below:
+          <br/>
+          <iframe title="PDF Report" src={NAOPDF} height="100%" width="30%">
+            <p>Your browser does not support iframes. <a href="path/to/yourfile.pdf">Download the PDF</a> instead.</p>
+          </iframe>
+        </p>
+      </div>
+      
+    </ContentBlock>
+
+    <ContentBlock>
+      <h1 className='sectionHeader' style={{ fontSize: '4rem'}}>Software Projects</h1>
+      <p className='contentSection'>
+        Quip goes here
+      </p>
+    </ContentBlock >
+
     <ContentBlock> 
       <div>
+        <h1 className='sectionHeader'>Quantum Engine</h1>
         <p className='contentSection'>
           This is a placeholder for the projects section. Work in Progress.
         </p>
       </div>
       
     </ContentBlock>
-    <p>
-      <a href="https://github.com/devanneil" target="_blank" rel="noopener noreferrer">
-        Visit my GitHub
-      </a>
-    </p>
-    <br />
-    <p>
-      <a href="www.linkedin.com/in/devan-neil" target="_blank" rel="noopener noreferrer">
-        Visit my LinkedIn
-      </a>
-    </p>
+
+    <ContentBlock> 
+      <div>
+        <h1 className='sectionHeader'>RoSE Lab</h1>
+        <p className='contentSection'>
+          This is a placeholder for the projects section. Work in Progress.
+        </p>
+      </div>
+      
+    </ContentBlock>
+
+    <ContentBlock> 
+      <div>
+        <h1 className='sectionHeader'>VR Research</h1>
+        <p className='contentSection'>
+          This is a placeholder for the projects section. Work in Progress.
+        </p>
+      </div>
+      
+    </ContentBlock>
+
+    <ContentBlock>
+      <h1 className='sectionHeader' style={{ fontSize: '4rem'}}>Miscellaneous Projects</h1>
+      <p className='contentSection'>
+        A mostly comprehensive list of everything else, probably.
+      </p>
+    </ContentBlock >
+
+    <ContentBlock> 
+      <div>
+        <h1 className='sectionHeader'>Freshman Design</h1>
+        <p className='contentSection'>
+          This is a placeholder for the projects section. Work in Progress.
+        </p>
+      </div>
+      
+    </ContentBlock>
+
+    <ContentBlock> 
+      <div>
+        <h1 className='sectionHeader'>3D Printer Build</h1>
+        <p className='contentSection'>
+          This is a placeholder for the projects section. Work in Progress.
+        </p>
+      </div>
+      
+    </ContentBlock>
+
+    <ContentBlock> 
+      <div>
+        <h1 className='sectionHeader'>Drone Build</h1>
+        <p className='contentSection'>
+          This is a placeholder for the projects section. Work in Progress.
+        </p>
+      </div>
+      
+    </ContentBlock>
 
   </div>
 }
